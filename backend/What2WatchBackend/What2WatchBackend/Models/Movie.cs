0420bcf38fcab7c735a26a3fb905ddfd9ab1c7c4
element2562 <element25668@gmail.com>
2018-10-30 09:32:05 -0500

    api complete

---
+++ backend/What2WatchBackend/What2WatchBackend/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace What2WatchBackend.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+
+        [Required]
+        public int MovieDbId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Summary { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+
+        public int? UserRating { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
